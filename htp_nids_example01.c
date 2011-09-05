70eae81ab766c26c2af82ce5e865e539b126ddaa
Mark Ellzey <mark.thomas@mandiant.com>
2011-09-05 15:06:07 -0400

    fix

--- htp_nids_example01.c
+++ htp_nids_example01.c
@@ -1,180 +1,214 @@
 #include <stdio.h>
-#include <assert.h> 
+#include <assert.h>
 #include <stdint.h>
 #include <string.h>
 #include <errno.h>
 #include <nids.h>
 #include <htp/htp.h>
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 struct ht_sniff_cfg {
-    htp_cfg_t *hcfg;
-    char *iface;
-    char *filter;
+    htp_cfg_t * hcfg;
+    char      * iface;
+    char      * filter;
 };
 
-struct ht_sniff_cfg *cfg;
+struct ht_sniff_cfg * cfg;
 
 int
-loghdr_cb(htp_connp_t *connp)
-{
-    char *method, 
-	 *host, 
-	 *agent,
-	 *proto,
-	 *referer,
-	 *uri;
-    htp_tx_t *tx;
-    htp_header_t *hdr;
+loghdr_cb(htp_connp_t * connp) {
+    char * method,
+    * host,
+    * agent,
+    * proto,
+    * referer,
+    * uri;
+    htp_tx_t     * tx;
+    htp_header_t * hdr;
 
-    host = method = proto = 
-	referer = uri = agent = NULL;
+    host = method = proto =
+                        referer = uri = agent = NULL;
 
     list_iterator_reset(connp->conn->transactions);
-    tx    = list_iterator_next(connp->conn->transactions);
+    tx     = list_iterator_next(connp->conn->transactions);
 
-    uri     = bstr_tocstr(tx->request_uri);
-    proto   = bstr_tocstr(tx->request_protocol);
-    method  = bstr_tocstr(tx->request_method);
-    
-    if ((hdr = table_getc(tx->request_headers, "host")))
-	host = bstr_tocstr(hdr->value);
+    uri    = bstr_tocstr(tx->request_uri);
+    proto  = bstr_tocstr(tx->request_protocol);
+    method = bstr_tocstr(tx->request_method);
 
-    if ((hdr = table_getc(tx->request_headers, "referer")))
-	referer = bstr_tocstr(hdr->value);
+    if ((hdr = table_getc(tx->request_headers, "host"))) {
+        host = bstr_tocstr(hdr->value);
+    }
 
-    if ((hdr = table_getc(tx->request_headers, "user-agent")))
-	agent = bstr_tocstr(hdr->value);
+    if ((hdr = table_getc(tx->request_headers, "referer"))) {
+        referer = bstr_tocstr(hdr->value);
+    }
 
-    printf("%s %s:%d \"%s http://%s%s %s\" \"%s\" \"%s\"\n", 
-	    connp->conn->remote_addr, 
-	    connp->conn->local_addr, 
-	    connp->conn->local_port,
-	    method, 
-	    host?host:connp->conn->remote_addr, 
-	    uri?uri:"", 
-	    proto,
-	    referer?referer:"-",
-	    agent?agent:"-");
-    
-    if (method) free(method);
-    if (proto) free(proto);
-    if (uri) free(uri);
-    if (referer) free(referer);
-    if (agent) free(agent);
-    if (host) free(host);
+    if ((hdr = table_getc(tx->request_headers, "user-agent"))) {
+        agent = bstr_tocstr(hdr->value);
+    }
+
+    printf("%s %s:%d \"%s http://%s%s %s\" \"%s\" \"%s\"\n",
+           connp->conn->remote_addr,
+           connp->conn->local_addr,
+           connp->conn->local_port,
+           method,
+           host ? host : connp->conn->remote_addr,
+           uri ? uri : "",
+           proto,
+           referer ? referer : "-",
+           agent ? agent : "-");
+
+    if (method) {
+        free(method);
+    }
+    if (proto) {
+        free(proto);
+    }
+    if (uri) {
+        free(uri);
+    }
+    if (referer) {
+        free(referer);
+    }
+    if (agent) {
+        free(agent);
+    }
+    if (host) {
+        free(host);
+    }
 
     return 0;
-}
-
+} /* loghdr_cb */
 
 void
-ht_sniff_cb(struct tcp_stream *a_tcp, void **pkt)
-{
-    htp_connp_t *connp;
-    struct timeval tv;
-    struct half_stream *hlf;
-    char *srcaddr, *dstaddr;
+ht_sniff_cb(struct tcp_stream * a_tcp, void ** pkt) {
+    htp_connp_t        * connp;
+    struct timeval       tv;
+    struct half_stream * hlf;
+    char               * srcaddr;
+    char               * dstaddr;
 
     gettimeofday(&tv, NULL);
 
-    switch(a_tcp->nids_state)
-    {
-	case NIDS_JUST_EST:
-	    srcaddr = strdup((char *)inet_ntoa(*(struct in_addr *)
-		    &a_tcp->addr.saddr));
-	    dstaddr = strdup((char *)inet_ntoa(*(struct in_addr *)
-		    &a_tcp->addr.daddr));
+    switch (a_tcp->nids_state) {
+        case NIDS_JUST_EST:
+            srcaddr = calloc(32, 1);
+            dstaddr = calloc(32, 1);
 
-	    /* initialize our connection pointer */
+            strncpy(srcaddr, (const char *)inet_ntoa(*((struct in_addr *)&a_tcp->addr.saddr)), 31);
+            strncpy(dstaddr, (const char *)inet_ntoa(*((struct in_addr *)&a_tcp->addr.daddr)), 31);
 
-	    assert((connp = htp_connp_create(cfg->hcfg)));
 
-	    htp_connp_open(connp, srcaddr, 432, dstaddr, 80, tv.tv_usec); 
+            /* initialize our connection pointer */
 
-	    free(srcaddr);
-	    free(dstaddr);
+            assert((connp = htp_connp_create(cfg->hcfg)));
 
-	    /* tell libnids to only watch the client data */
-	    a_tcp->server.collect = 1;
-	    a_tcp->user = connp;
-	    break;
-	case NIDS_TIMED_OUT:
-	case NIDS_CLOSE:
-	case NIDS_EXITING:
-	case NIDS_RESET:
-	    if ((connp = a_tcp->user))
-	    {
-		htp_connp_close(connp, tv.tv_usec);
-		htp_connp_destroy_all(connp);
-	    }
-	    break;
-	case NIDS_DATA:
-	    if (!(connp = a_tcp->user))
-		/* yuck */
-		break;
+            htp_connp_open(connp, srcaddr, 432, dstaddr, 80, tv.tv_usec);
 
-	    if (!(hlf = &a_tcp->server))
-		/* yuck2 */
-		break;
+            free(srcaddr);
+            free(dstaddr);
 
-	    if (htp_connp_req_data(connp, tv.tv_usec,
-			hlf->data, hlf->count) == HTP_ERROR)
-		break;
+            /* tell libnids to only watch the client data */
+            a_tcp->server.collect = 1;
+            a_tcp->user           = connp;
+            break;
+        case NIDS_TIMED_OUT:
+        case NIDS_CLOSE:
+        case NIDS_EXITING:
+        case NIDS_RESET:
+            if ((connp = a_tcp->user)) {
+                htp_connp_close(connp, tv.tv_usec);
+                htp_connp_destroy_all(connp);
+            }
+            break;
+        case NIDS_DATA:
+            if (!(connp = a_tcp->user)) {
+                /* yuck */
+                break;
+            }
 
-	    nids_discard(a_tcp, hlf->count);
-	    break;
-	default:
-	    /* yuck */
-	    break;
-    }
+            if (!(hlf = &a_tcp->server)) {
+                /* yuck2 */
+                break;
+            }
 
-}
-	    
+            if (htp_connp_req_data(connp, tv.tv_usec,
+                                   hlf->data, hlf->count) == HTP_ERROR) {
+                break;
+            }
+
+            nids_discard(a_tcp, hlf->count);
+            break;
+        default:
+            /* yuck */
+            break;
+    } /* switch */
+}     /* ht_sniff_cb */
+
 int
-parse_args(struct ht_sniff_cfg *cfg, 
-	int argc, char **argv)
-{
-    extern char *optarg;
-    extern int   optind,
-	         opterr,
-		 optopt;
-    int          c;
+parse_args(struct ht_sniff_cfg * cfg,
+           int argc, char ** argv) {
+    extern char * optarg;
+    extern int    optind,
+                  opterr,
+                  optopt;
+    int           c;
 
-    static char *help = 
-	"Options: \n"
-	" -h:         Derr...\n"
-	" -i <iface>: Interface to sniff\n"
-	" -l <fmt>  : Log format\n"
-	" -f <bpf>:   BPF Filter\n";
+    static char * help =
+        "Options: \n"
+        " -h:         Derr...\n"
+        " -i <iface>: Interface to sniff\n"
+        " -l <fmt>  : Log format\n"
+        " -f <bpf>:   BPF Filter\n";
 
     cfg->iface  = "eth0";
     cfg->filter = "tcp port 80";
 
-    while((c = getopt(argc, argv, "hi:f:l:")) != -1)
-    {
-	switch(c)
-	{
-	    case 'i':
-		cfg->iface  = optarg;
-		break;
-	    case 'f':
-		cfg->filter = optarg;
-		break;
-	    case 'h':
-	    default:
-		printf("Usage: %s [opts\n%s",
-			argv[0], help);
-		exit(1);
-	}
+    while ((c = getopt(argc, argv, "hi:f:l:")) != -1) {
+        switch (c) {
+            case 'i':
+                cfg->iface = optarg;
+                break;
+            case 'f':
+                cfg->filter = optarg;
+                break;
+            case 'h':
+            default:
+                printf("Usage: %s [opts\n%s",
+                       argv[0], help);
+                exit(1);
+        }
     }
 
     return 0;
 }
 
 void
-ht_nids_init(struct ht_sniff_cfg *cfg)
-{
+ht_sniff_init(struct ht_sniff_cfg * cfg) {
+    assert((cfg->hcfg = htp_config_create()));
+
+    htp_config_set_server_personality(cfg->hcfg, HTP_SERVER_IDS);
+    htp_config_register_request_headers(cfg->hcfg, loghdr_cb);
+}
+
+int
+main(int argc, char ** argv) {
     struct nids_chksum_ctl ctl;
+
+    if (!(cfg = calloc(sizeof(struct ht_sniff_cfg), 1))) {
+        perror(strerror(errno));
+    }
+
+    if (!(cfg->hcfg = htp_config_create())) {
+        perror(strerror(errno));
+    }
+
+    parse_args(cfg, argc, argv);
+    ht_sniff_init(cfg);
 
     /* turn off the libnids scanning stuff */
     nids_params.scan_num_hosts  = 0;
@@ -182,47 +216,21 @@ ht_nids_init(struct ht_sniff_cfg *cfg)
     nids_params.pcap_filter     = cfg->filter;
     nids_params.device          = cfg->iface;
 
-    assert(nids_init());
+    if (!nids_init()) {
+        printf("%s\n", nids_errbuf);
+        exit(1);
+    }
 
     /* we want to turn off the checksum checking since
-       many new interfaces include checksum offloading */
+     *  many new interfaces include checksum offloading */
     ctl.netaddr = 0;
     ctl.mask    = 0;
     ctl.action  = NIDS_DONT_CHKSUM;
 
     nids_register_chksum_ctl(&ctl, 1);
     nids_register_tcp(ht_sniff_cb);
-}
-
-void
-ht_sniff_init(struct ht_sniff_cfg *cfg)
-{
-    assert((cfg->hcfg = htp_config_create()));
-
-    htp_config_set_server_personality(cfg->hcfg,
-	    HTP_SERVER_IDS);
-    htp_config_register_request_headers(cfg->hcfg, 
-	    loghdr_cb);
-}
-
-int main(int argc, char **argv)
-{
-    if (!(cfg = calloc(sizeof(struct ht_sniff_cfg), 1)))
-	perror(strerror(errno));
-    
-    if (!(cfg->hcfg = htp_config_create()))
-	perror(strerror(errno));
-
-    parse_args(cfg, argc, argv);
-    ht_nids_init(cfg);
-    ht_sniff_init(cfg);
-
     nids_run();
 
     return 0;
 }
-
-	
-
-
 
